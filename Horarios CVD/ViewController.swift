5141ef1aeaee5e69c84ad1f6fc6d4475e3bf00a8
Javier López Achondo <javier@MacBook-Pro-de-Javier-2.local>
2016-03-10 18:59:04 -0300

    first commit

---
+++ Horarios CVD/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Horarios CVD
+//
+//  Created by Javier López Achondo on 10-03-16.
+//  Copyright © 2016 Javier López Achondo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
