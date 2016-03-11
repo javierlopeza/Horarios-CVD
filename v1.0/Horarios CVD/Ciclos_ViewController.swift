bf84f22c7c4d55078fe7aa112625fd3858fd09a5
Javier López Achondo <javier@MacBook-Pro-de-Javier.2>
2016-03-11 12:38:32 -0300

    v1.1 Download Buttotton - Some Fixes

--- v1.0/Horarios CVD/Ciclos_ViewController.swift
+++ v1.0/Horarios CVD/Ciclos_ViewController.swift
@@ -12,13 +12,20 @@ class Ciclos_ViewController: UIViewController {
 
     @IBOutlet var BGImage: UIImageView!
     
+    var BGImageNumber = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        BGImage.image = UIImage(named: "\(Int(arc4random_uniform(3) + 1))")
+        // Se elije un numero random de foto.
+        BGImageNumber = Int(arc4random_uniform(30) + 1)
+        BGImage.image = UIImage(named: "\(BGImageNumber)")
         
     }
 
+    @IBAction func DescargarPressed(sender: AnyObject) {
+        UIImageWriteToSavedPhotosAlbum(UIImage(named: "\(BGImageNumber)")!, nil, nil, nil)
+    }
 }
 
